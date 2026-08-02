5d9a25918330b1ccf341d5f3ac8a33b3a9a68f91
Uzair047C <uzairsahi096@gmail.com>
2026-08-02 22:33:42 +0500

    inheritance/Animal.py

---
+++ opp_py/inheritance/Animal.py
@@ -0,0 +1,10 @@
+class Animals:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def eat(self):
+        print(f"{self.name} is eating whose age is {self.age}.")
+    def sleep(self):
+        print(f"{self.name} is sleeping whose age is {self.age}.")
+    
+
